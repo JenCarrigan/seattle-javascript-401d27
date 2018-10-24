071b7a90c8405214987125453095b9a24024c259
Jen Carrigan <jen.d.carrigan@gmail.com>
2018-10-24 08:35:29 -0700

    lab 7

---
+++ 07-vanilla-http-server/lab/src/lib/bodyParser.js
@@ -0,0 +1,22 @@
+
+module.exports = (req) => {
+  return new Promise( (resolve, reject) => {
+    let text = '';
+  
+    req.on('data', (buffer) => {
+      text += buffer.toString();
+    });
+    
+    req.on('end', () => {
+      try {
+        req.body = JSON.parse(text);
+        resolve(req);
+      }
+      catch(err) {
+        reject(err);
+      }
+    });
+  });
+  
+
+};
